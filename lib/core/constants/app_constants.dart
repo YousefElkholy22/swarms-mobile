bcea7194808f4ac441f0844980a20c124fe9993f
Ahmed Arafat <ahmed.arafat3535@gmail.com>
2024-09-19 15:57:55 +0300

    feat: build onboarding data

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,5 +1,22 @@
+import '../../features/welcome/data/models/onboarding_entity.dart';
+import '../../resources/constants/app_assets.dart';
+import '../localization/generated/l10n.dart';
+
 class AppConstants {
   AppConstants._();
+
+  static final onboardingContents = [
+    OnBoardingEntity(
+      title: S.current.onboardingTitle1,
+      description: S.current.onboardingSubtitle1,
+      image: Assets.imagesOnboarding1,
+    ),
+    OnBoardingEntity(
+      title: S.current.onboardingTitle2,
+      description: S.current.onboardingSubtitle2,
+      image: Assets.imagesOnboarding2,
+    ),
+  ];
 }
 
 class PrefsKeys {
